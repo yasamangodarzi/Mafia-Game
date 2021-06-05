2e6ef2e59cce607f8e4279d95ca71c6023fb4481
yasamangodarzi <yaasgodarzi@gmail.com>
2021-06-05 11:59:40 -0700

    create Client and Server and port

--- Client/Client.java
+++ Client/Client.java
@@ -1,10 +1,70 @@
 package Client;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class Client {
 
+        private int port;
+        private String userName;
+
+        public Client(int port) {
+                this.port = port;
+        }
+        public void execute() {
+                try {
+                        Socket socket = new Socket("localhost", port);
+
+                        System.out.println("Welcome to the Mafia game");
+                        System.out.println("The mafia game is actually a game between two groups, the citizens and the mafia.\n" +
+                                "The game continues in a cycle of day and night.\n" +
+                                " When people present in the game try to recognize the Mafia and eliminate it from the game Mafia must hide their role altogether or distract others by distracting them. \n" +
+                                "The mafia must work well together and have the right strategy for the people who want to kill at night.\n" +
+                                " The main task of the citizens in this game is to realize the role of the Mafia in spite of all the lies they tell.\n");
+
+
+
+                } catch (UnknownHostException ex) {
+                        System.out.println("Server not found ");
+                } catch (IOException ex) {
+                        System.out.println("IOException Error");
+                }
+
+        }
+
+        public String getUserName() {
+                return userName;
+        }
+
+        public void setUserName(String userName) {
+                this.userName = userName;
+        }
 
         public static void main(String[] args) {
-            // write your code here
+                Scanner scanner=new Scanner(System.in);
+                Integer port = null;
+                do {
+
+                        System.out.println("\n\nPlease enter port game: ");
+
+                        String s = scanner.nextLine();
+
+                        try {
+
+                                port = Integer.parseInt(s);
+
+                        } catch (NumberFormatException e) {
+
+                                System.out.println("ERROR: " + s + " is not a number.");
+                        }
+
+                } while (port == null);
+
+               Client client = new  Client(port);
+                client.execute();
+
         }
 
 
--- Server/Server.java
+++ Server/Server.java
@@ -1,8 +1,36 @@
 package Server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Server {
+    private static  int port=5000;
+
+    public Server(int port) {
+        this.port = port;
+    }
+
+    public void execute() {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                Socket socket = serverSocket.accept();
+
+            }
+
+        } catch (IOException ex) {
+            System.out.println("Error in the server");
+        }
+    }
+
     public static void main(String[] args) {
-        // write your code here
+
+        System.out.println("If you want a new game, enter the port :" + port);
+        Scanner scanner=new Scanner(System.in);
+        Integer porrt= Integer.valueOf(scanner.nextLine());
+         Server server = new  Server(porrt);
+        server.execute();
     }
 
 }
